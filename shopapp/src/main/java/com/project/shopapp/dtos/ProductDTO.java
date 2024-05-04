9c5094af837ca2e48418625e7126758d103994ef
haruki-dayooo <ducluongdn3@gmail.com>
2024-05-04 21:25:11 +0700

    Section 3 lecture 17

---
+++ shopapp/src/main/java/com/project/shopapp/dtos/ProductDTO.java
@@ -0,0 +1,28 @@
+package com.project.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductDTO {
+    @NotBlank(message = "Tittle is required!")
+    @Size(min = 3, max = 200, message = "Tittle must be between 3 to 200 characters!")
+    private String name;
+
+    @Min(value = 0, message = "Price must be greater than or equal to 0!")
+    @Max(value = 100000000, message = "Price must be lesser than or equal to 100000000!")
+    private float price;
+
+    private String thumbnail;
+    private String description;
+    @JsonProperty("category_id")
+    private String categoryId;
+}
