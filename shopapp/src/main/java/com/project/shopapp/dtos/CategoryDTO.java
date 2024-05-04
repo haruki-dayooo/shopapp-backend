46d7953c86edccd021a0801d37d26c3504218982
haruki-dayooo <ducluongdn3@gmail.com>
2024-05-04 20:39:50 +0700

    Section 3 lecture 16

---
+++ shopapp/src/main/java/com/project/shopapp/dtos/CategoryDTO.java
@@ -0,0 +1,14 @@
+package com.project.shopapp.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoryDTO {
+    @NotEmpty(message = "Category name cannot be empty")
+    private String name;
+}
