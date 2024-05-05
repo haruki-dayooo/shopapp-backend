a1dce7ae434639b489175c47727b7eb63a72147d
haruki-dayooo <ducluongdn3@gmail.com>
2024-05-05 23:36:14 +0700

    Section 3 lecture 20

--- shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
+++ shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
@@ -1,17 +1,16 @@
 package com.project.shopapp.controllers;
 
 import com.project.shopapp.dtos.UserDTO;
+import com.project.shopapp.dtos.UserLoginDTO;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,9 +18,20 @@ import java.util.List;
 public class UserController {
 
     @PostMapping("/register")
-    public ResponseEntity<?> createUser(@Valid @RequestBody UserDTO user) {
+    public ResponseEntity<?> createUser(
+            @Valid @RequestBody UserDTO user,
+            BindingResult result) {
         try {
-
+            if (result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            if(!user.getPassword().equals(user.getConfirmPassword())) {
+                return ResponseEntity.badRequest().body("Password does not match!");
+            }
             return ResponseEntity.ok("Register successfully!");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
