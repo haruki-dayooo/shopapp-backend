1ef780ede29c6b9f74af5aeab1b8b77a12774be4
haruki-dayooo <ducluongdn3@gmail.com>
2024-05-07 10:53:26 +0700

    Section 3 lecture 24

---
+++ shopapp/src/main/java/com/project/shopapp/models/Category.java
@@ -0,0 +1,21 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
---
+++ shopapp/src/main/java/com/project/shopapp/models/Product.java
@@ -0,0 +1,31 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 350)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Float price;
+
+    @Column(name = "thumbnail", length = 300)
+    private String thumbnail;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category categoryId;
+}
